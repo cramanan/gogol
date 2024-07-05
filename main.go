9776823a857bc48cd9388b68abf55e09fe3d463e
cramanan <cyrilramananjaona@gmail.com>
2024-07-05 10:17:29 +0200

    removing update for now

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/cramanan/gogol/cmd"
 	_ "github.com/cramanan/gogol/cmd/languages"
-	_ "github.com/cramanan/gogol/cmd/scripts"
 	_ "github.com/cramanan/gogol/cmd/utilities"
 )
 
